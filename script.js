47a919ecdc3d18d296c80ac16b4b38c05daee535
nico-von <131730901+nico-von@users.noreply.github.com>
2024-02-06 18:10:01 +0800

    Write a new recursive checker

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const MAX_ROWS_COLS = 6;
 const BLOCKS_NEEDED_TO_WIN = 3;
+const ROW_DATA_ATTRIB = "data-row-number";
+const COL_DATA_ATTRIB = "data-col-number";
 
-const game = (function () {
+const game = (function (maxRowsCols, blocksNeededToWin) {
     const gameboard = [];
-    const moves = [-1, 1];
+    const moves = ["X", "O"];
+    let lastMoveIndex = 0;
 
     function _getFromBoard(row, col) {
         if (!gameboard[row]) {
@@ -23,61 +26,41 @@ const game = (function () {
         gameboard[row][col] = move;
         return;
     }
+    function _getNextMove() {
+        let nextMoveIndex = lastMoveIndex == 0 ? 1 : 0;
+        return {
+            nextMove: moves[nextMoveIndex],
+            nextMoveIndex
+        };
+    }
 
-    function addMove(move, row, col) {
-        if (row >= MAX_ROWS_COLS || col >= MAX_ROWS_COLS) {
+    function _commitNextMove(nextMoveIndex) {
+        lastMoveIndex = nextMoveIndex;
+    }
+
+    function addMove(row, col) {
+        let { nextMove, nextMoveIndex } = _getNextMove();
+        if (row >= maxRowsCols || col >= maxRowsCols) {
             return;
         }
-        if (!moves.includes(move)) {
+        if (!moves.includes(nextMove)) {
             return;
         }
         if (_getFromBoard(row, col)) {
             return;
         }
-        _addToBoard(move, row, col);
-        return checkBoard();
+        _addToBoard(nextMove, row, col);
+        _commitNextMove(nextMoveIndex);
+        return _checkBoard(nextMove);
+    }
+    function _recursiveCheck(row, col) {
     }
 
-    //recursive board check
-    function _checkRowsCols(row, col, direction = "rows", total) {
-        //base case
-        if (Math.abs(total) == BLOCKS_NEEDED_TO_WIN) {
-            return total / BLOCKS_NEEDED_TO_WIN;
-        }
-
-        const currCell = _getFromBoard(row, col);
-        if (!currCell) return;
-
-        if (direction == "row") {
-            return _checkRowsCols(row, col + 1, direction, currCell + total);
-        } else if (direction == "col") {
-            return _checkRowsCols(row + 1, col, direction, currCell + total);
-        } else if (direction == "diag-left") {
-            return _checkRowsCols(row + 1, col + 1, direction, currCell + total);
-        } else if (direction == "diag-right") {
-            return _checkRowsCols(row - 1, col + 1, direction, currCell + total);
-        }
-    }
-
-    function checkBoard() {
+    function _checkBoard(moved) {
         // check rows and cols
-        for (let i = 0; i < MAX_ROWS_COLS; i++) {
-            let winnerRow = _checkRowsCols(i, 0, "row", 0);
-            if (winnerRow) { 
-                return winnerRow 
-            }
-
-            let winnerCol = _checkRowsCols(0, i, "col", 0);
-            if (winnerCol) { 
-                return winnerCol 
-            }
-        }
-        // check diag-left
-        let winnerDiagLeft = _checkRowsCols(0, 0, "diag-left", 0);
-        if (winnerDiagLeft) return winnerDiagLeft
-        // check diag-right
-        let winnerDiagRight = _checkRowsCols(MAX_ROWS_COLS, 0, "diag-right", 0);
-        if (winnerDiagRight) return winnerDiagRight
+        let winner = null;
+        winner = _recursiveCheck(0,0);
+        return {moved, winner}
     }
 
     function resetBoard() {
@@ -87,7 +70,64 @@ const game = (function () {
     return {
         gameboard,
         addMove,
-        resetBoard,
+        resetBoard
     }
 
-})()
+})(MAX_ROWS_COLS, BLOCKS_NEEDED_TO_WIN)
+
+const domHandling = (function (maxRowsCols) {
+    function _render(child) {
+        document.body.append(child);
+    }
+
+    function _cellClickHandler(e) {
+        let rowDiv = e.currentTarget;
+        let colDiv = e.target;
+        if (!(rowDiv.hasAttribute(ROW_DATA_ATTRIB) && colDiv.hasAttribute(COL_DATA_ATTRIB))) {
+            return
+        }
+        let row = rowDiv.getAttribute(ROW_DATA_ATTRIB);
+        let col = colDiv.getAttribute(COL_DATA_ATTRIB);
+        let move = game.addMove(row, col);
+        console.log(move)
+        if (!move) {
+            return;
+        }
+
+        colDiv.textContent = move.moved;
+        if (move.result) {
+            alert(`${move.result} won!`)
+        }
+    }
+
+    function createBoard() {
+        let board = document.createElement("div");
+        board.classList.add("board");
+        for (let i = 0; i < maxRowsCols; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            row.setAttribute(ROW_DATA_ATTRIB, i);
+            row.addEventListener("click", _cellClickHandler);
+            board.appendChild(row);
+            for (j = 0; j < maxRowsCols; j++) {
+                let col = document.createElement("div");
+                col.classList.add("col");
+                col.setAttribute(COL_DATA_ATTRIB, j);
+                row.appendChild(col);
+            }
+        }
+        _render(board);
+    }
+    return {
+        createBoard,
+    }
+
+})(MAX_ROWS_COLS)
+
+function main() {
+    domHandling.createBoard();
+}
+
+window.onload = main;
+
+
